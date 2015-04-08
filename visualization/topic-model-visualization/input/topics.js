50468a23d00f9674b397bd56b30351eb26c4d2e8
Vu <vu@example.com>
2015-04-07 23:17:55 -0700

    Add visualization

---
+++ visualization/topic-model-visualization/input/topics.js
@@ -0,0 +1,23 @@
+var topics=[
+{
+  words: ["temporal","time","graphs","uniform","graph"],
+  word_weights: [0.789,0.457,0.237,0.237,0.237],
+  docs: ["Moving in temporal graphs with very sparse random availability of edges. (arXiv:1310.7898v1 [cs.DS])","Wireless Energy Harvesting and Information Relaying: Adaptive Time-Switching Protocols and Throughput Analysis. (arXiv:1310.7648v1 [cs.IT])","Max-Flows on Sparse and Dense Networks. (arXiv:1309.2525v3 [cs.DS] UPDATED)","Random Access to Grammar Compressed Strings. (arXiv:1001.1565v3 [cs.DS] UPDATED)","A New Push-Relabel Algorithm for the Maximum Flow Problem. (arXiv:1310.7840v1 [cs.DS])"],
+  doc_weights: [23.703,4.572,2.743,2.286,2.286],
+  doc_urls: ["http://arxiv.org/abs/1310.7898","http://arxiv.org/abs/1310.7648","http://arxiv.org/abs/1309.2525","http://arxiv.org/abs/1001.1565","http://arxiv.org/abs/1310.7840"]
+},
+{
+  words: ["algorithms","functions","algorithm","convergence","sars"],
+  word_weights: [0.789,0.457,0.237,0.237,0.237],
+  docs: ["On Proving Linear Convergence of Comparison-based Step-size Adaptive Randomized Search on Scaling-Invariant Functions via Stability of Markov Chains. (arXiv:1310.7697v1 [cs.NA])","Conditional Entropy based User Selection for Multiuser MIMO Systems. (arXiv:1310.7852v1 [cs.IT])","The Information Geometry of Mirror Descent. (arXiv:1310.7780v1 [stat.ML])","Optimal cooperative motion planning for vehicles at intersections. (arXiv:1310.7729v1 [cs.SY])","Achieving maximum energy-efficiency in multi-relay OFDMA cellular networks: a fractional programming approach. (arXiv:1310.7839v1 [cs.IT])"],
+  doc_weights: [0.545,0.529,0.426,0.348,0.348],
+  doc_urls: ["http://arxiv.org/abs/1310.7697","http://arxiv.org/abs/1310.7852","http://arxiv.org/abs/1310.7780","http://arxiv.org/abs/1310.7729","http://arxiv.org/abs/1310.7839"]
+},
+{
+  words: ["networks","knowledge","signed","structure","analysis"],
+  word_weights: [0.829,0.456,0.240,0.163,0.143],
+  docs: ["Graph Theoretic Analysis of Knowledge Networks. (arXiv:1305.1852v2 [cs.SI] UPDATED)","Overlapping community detection in signed networks. (arXiv:1310.4023v2 [cs.SI] UPDATED)","Connectivity of confined 3D Networks with Anisotropically Radiating Nodes. (arXiv:1310.7473v1 [cs.IT] CROSS LISTED)","On the evolution of interaction networks: primitive typology of vertex and prominence of measures. (arXiv:1310.7769v1 [cs.SI])","Predicting Trends in Social Networks via Dynamic Activeness Model. (arXiv:1308.1995v2 [cs.SI] UPDATED)"],
+  doc_weights: [15.720,10.111,3.604,3.318,2.488],
+  doc_urls: ["http://arxiv.org/abs/1305.1852","http://arxiv.org/abs/1310.4023","http://arxiv.org/abs/1310.7473","http://arxiv.org/abs/1310.7769","http://arxiv.org/abs/1308.1995"]
+}
+];
